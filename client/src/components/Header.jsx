a63b3b0eb828b0f138be6fa1cc80e3607392576d
aniketsonkerr <aniketsonkerr@gmail.com>
2025-03-07 17:38:49 +0530

    made it responsive

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,46 +1,100 @@
-import AppBar from "@mui/material/AppBar";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { Menu } from "lucide-react";
+
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <>
-      <div className=" flex flex-row justify-between items-center text-2xl mx-10">
-        <div className="w-1/5 flex justify-center h-auto">
-          <Link style={{ textDecoration: "none" }} to={"/"}>
+      <div className="flex justify-between items-center px-5 md:px-10 py-4 bg-white shadow-md">
+        {/* Logo */}
+        <div className="w-auto flex justify-center">
+          <Link to="/" className="flex items-center">
             <img
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXXnj2GoesFevhFux9wUP9njlUE4BhkaimVw51jQMgn90YSlbozPyP60MkR5CTlEA2eK8&usqp=CAU"
               alt="logo"
-              className=" h-32 rounded-2xl "
-            ></img>
+              className="h-20 md:h-32 rounded-2xl"
+            />
           </Link>
         </div>
-        <ul className=" flex md:flex-row justify-between w-1/2 flex-col font-semibold text-gray-600 items-center">
-          <li className="hover:text-3xl duration-300 ease-in-out">
+
+        {/* Desktop Menu */}
+        <ul className="hidden md:flex gap-8 font-semibold text-gray-600 items-center">
+          <li className="hover:text-black transition-all duration-300">
+            <Link className="no-underline text-inherit" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="hover:text-black transition-all duration-300">
+            <Link className="no-underline text-inherit" to="/about">
+              About
+            </Link>
+          </li>
+          <li className="hover:text-black transition-all duration-300">
+            <Link className="no-underline text-inherit" to="/contact">
+              Contact
+            </Link>
+          </li>
+          <li className="bg-red-600 text-white px-4 py-2 rounded-md hover:scale-105 transition-all duration-300">
+            <Link className="no-underline text-inherit" to="/portfolio">
+              Portfolio
+            </Link>
+          </li>
+        </ul>
+
+        {/* Mobile Menu Button */}
+        <button
+          className="md:hidden"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          <Menu size={28} />
+        </button>
+      </div>
+
+      {/* Mobile Menu Dropdown */}
+      {isMenuOpen && (
+        <ul className="md:hidden flex flex-col items-center bg-white shadow-lg py-4 space-y-4 text-gray-600">
+          <li>
             <Link
-              className="no-underline text-inherit hover:text-black"
-              to={"/"}
+              className="no-underline text-inherit"
+              to="/"
+              onClick={() => setIsMenuOpen(false)}
             >
               Home
             </Link>
           </li>
-          <li className="hover:text-3xl duration-300 ease-in-out">
-            <Link className="no-underline text-inherit hover:text-black">
+          <li>
+            <Link
+              className="no-underline text-inherit"
+              to="/about"
+              onClick={() => setIsMenuOpen(false)}
+            >
               About
             </Link>
           </li>
-          <li className="hover:text-3xl duration-300 ease-in-out">
+          <li>
             <Link
-              className="no-underline text-inherit hover:text-black"
-              to={"/contact"}
+              className="no-underline text-inherit"
+              to="/contact"
+              onClick={() => setIsMenuOpen(false)}
             >
               Contact
             </Link>
           </li>
-          <li className="hover:text-3xl duration-300 ease-in-out bg-red-600 text-white p-2">
-            <Link className="no-underline text-inherit">Portfolio</Link>
+          <li className="bg-red-600 text-white px-4 py-2 rounded-md">
+            <Link
+              className="no-underline text-inherit"
+              to="/portfolio"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              Portfolio
+            </Link>
           </li>
         </ul>
-      </div>
-      <hr className="mx-9" />
+      )}
+
+      <hr className="mx-5 md:mx-10" />
     </>
   );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,64 +1,68 @@
 import Carousel from "react-bootstrap/Carousel";
 import { Link } from "react-router-dom";
+
 function Home() {
   return (
     <>
-      <article className="flex flex-row  justify-center items-center h-svh mx-10">
-        <section className="w-2/5 font-semibold text-xl flex-wrap mr-10 flex flex-col justify-center h-svh items-center ">
-          <p className="font-light ">
+      <article className="flex flex-col md:flex-row justify-center items-center min-h-screen px-5 md:px-10 gap-8">
+        <section className="w-full md:w-2/5 font-semibold text-lg md:text-xl flex flex-col items-center text-center md:text-left">
+          <p className="font-light leading-relaxed">
             Make-It is your ideal art department & creative services partner,
             specializing in bespoke art fabrication, sculpture,
-            <br></br>
-            <br></br>
+            <br />
+            <br />
             prop making, window displays & visual merchandise design.
-            <br></br>
-            <br></br>
+            <br />
+            <br />
             Our South London workshop provides excellent design consultancy and
             art manufacture for a variety of art and design projects, using a
             wide range of materials to make your project happen.
-            <br></br>
-            <br></br>
+            <br />
+            <br />
             <b>
               Do you need something made?
-              <br></br>
+              <br />
               Email: info@make-it.london
-              <br></br>
+              <br />
               Tel: 02087789303
-              <br></br>
+              <br />
             </b>
           </p>
-          <Link className="no-underline text-inherit">
-            <button className=" bg-red-600 text-white p-3 text-lg  hover:text-2xl duration-300 ease-in-out drop-shadow-lg">
+          <Link className="no-underline text-inherit mt-4">
+            <button className="bg-red-600 text-white px-5 py-3 text-lg md:text-xl transition-all duration-300 hover:scale-105 shadow-lg">
               View Work Portfolio
             </button>
           </Link>
         </section>
-        <Carousel className="w-[789px] h-svh">
-          <Carousel.Item>
-            <img
-              src="https://images.squarespace-cdn.com/content/v1/5489ccbce4b05cd65fee1cc1/1540116449261-NDS6VVENJJGC2E775TQ4/DSC_1420.jpg?format=1000w"
-              alt="pic2"
-              className="w-[789px] h-svh object-cover"
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              src="https://images.squarespace-cdn.com/content/v1/5489ccbce4b05cd65fee1cc1/1603104850444-IWHAXF38M5UMYB2XJB4M/THIS-EARTH-OF-MAJESTY%2C-THIS-SEAT-OF-MARS_3stl_right-yellow.jpg?format=1000w"
-              alt="pic3"
-              className="w-[789px] h-svh object-cover"
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              src="https://images.squarespace-cdn.com/content/v1/5489ccbce4b05cd65fee1cc1/1727352775615-UQAKRD9B76WLG39BA6U4/%25C2%25A9%2BLibby%2BHeaney%252C%2BEnt-%2B%2528non-earthly%2Bdelights%2529%252C%2B2024.%2BCourtesy%2Bof%2BGazelli%2BArt%2BHouse%2B%2526%2BLibby%2BHeaney.%2BPhotography%2Bby%2BDeniz%2BGuzel.%2B%25288%2529.jpg?format=1000w"
-              alt="pic4"
-              className="w-[789px] h-svh object-cover"
-            />
-          </Carousel.Item>
-        </Carousel>
+
+        <div className="w-full md:w-[789px] h-[400px] md:h-svh">
+          <Carousel className="h-full">
+            <Carousel.Item>
+              <img
+                src="https://images.squarespace-cdn.com/content/v1/5489ccbce4b05cd65fee1cc1/1540116449261-NDS6VVENJJGC2E775TQ4/DSC_1420.jpg?format=1000w"
+                alt="pic2"
+                className="w-full h-full object-cover"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                src="https://images.squarespace-cdn.com/content/v1/5489ccbce4b05cd65fee1cc1/1603104850444-IWHAXF38M5UMYB2XJB4M/THIS-EARTH-OF-MAJESTY%2C-THIS-SEAT-OF-MARS_3stl_right-yellow.jpg?format=1000w"
+                alt="pic3"
+                className="w-full h-full object-cover"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                src="https://images.squarespace-cdn.com/content/v1/5489ccbce4b05cd65fee1cc1/1727352775615-UQAKRD9B76WLG39BA6U4/%25C2%25A9%2BLibby%2BHeaney%252C%2BEnt-%2B%2528non-earthly%2Bdelights%2529%252C%2B2024.%2BCourtesy%2Bof%2BGazelli%2BArt%2BHouse%2B%2526%2BLibby%2BHeaney.%2BPhotography%2Bby%2BDeniz%2BGuzel.%2B%25288%2529.jpg?format=1000w"
+                alt="pic4"
+                className="w-full h-full object-cover"
+              />
+            </Carousel.Item>
+          </Carousel>
+        </div>
       </article>
-      <hr className="mx-10"></hr>
-      <article></article>
+
+      <hr className="mx-5 md:mx-10 my-5"></hr>
     </>
   );
 }
